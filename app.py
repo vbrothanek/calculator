871c729f65813157ed6f819aa680fe28dfd1aee4
Vojtech Brothanek <vbrothanek@brotel.cz>
2023-05-03 20:03:59 +0200

    Upravil jsem main.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 def main():
-  print("KUKU!")
   name  = input("Zadej svoje jmeno")
   print(f"Hello {name}!")
 
 if __name__ == "__main__":
   main()
   print("Zatim tomu rozumim :)")
+  print("tady neco upravim")
+
