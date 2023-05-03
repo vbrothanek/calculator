1de2dda56a7f1adf27e91e5a0122418d321a9fa5
Vojtech Brothanek <vbrothanek@brotel.cz>
2023-05-03 20:06:25 +0200

    Pridal jsem novou funkci pro scitani dvou cisel.

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ def main():
   name  = input("Zadej svoje jmeno")
   print(f"Hello {name}!")
 
+def secti_dve_cisla(a, b):
+  return a + b
+
 if __name__ == "__main__":
   main()
   print("Zatim tomu rozumim :)")
-  print("tady neco upravim")
+  secti_dve_cisla(173, 11)
+
 
