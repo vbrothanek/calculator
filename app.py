612cff324428711d00f0f5d1be6ad7098c6a28ae
Vojtech Brothanek <vbrothanek@brotel.cz>
2023-05-03 20:21:03 +0200

    testik

--- app.py
+++ app.py
@@ -8,9 +8,13 @@ def secti_dve_cisla(a, b):
 def odecti_dve_cisla(a, b):
   return a - b
 
+def vydel_dve_cisla(a, b):
+  return a / b
+
 if __name__ == "__main__":
   main()
   print("Zatim tomu rozumim :)")
   secti_dve_cisla(173, 11)
-
+  odecti_dve_cisla(173, 11)
+  vydel_dve_cisla(173, 11)
 
